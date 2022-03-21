8d7f0a455cfd572564bbaf3731a5fa4ce01c9639
Samuel Holmes <47476255+HOLMESsamuel@users.noreply.github.com>
2022-03-21 12:08:57 +0100

    format returned json

--- main.py
+++ main.py
@@ -1,18 +1,12 @@
-# This is a sample Python script.
-
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-from api_requests import retrieve_block
 import json
 
-
-def print_hi():
-    print(retrieve_block())
-    print(json.dumps(retrieve_block(), indent=4))
+from api_requests import retrieve_block
 
 
-# Press the green button in the gutter to run the script.
+def print_block():
+    block_json_data = json.loads(retrieve_block())
+    print(json.dumps(block_json_data, indent=4))
+
+
 if __name__ == '__main__':
-    print_hi()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    print_block()
