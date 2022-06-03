dcad3257b5b758e84850f5bb71d05d0610f40f7c
Samuel Holmes <47476255+HOLMESsamuel@users.noreply.github.com>
2022-06-03 14:44:46 +0200

    add pdf writer module

--- printing.py
+++ printing.py
@@ -1,17 +1,31 @@
 # Define all methods to print objects
+from fpdf import FPDF
+
 import constants
 
 
+# Python program to create
+# a pdf file
+
+
 def print_blocks(blocks):
+    # save FPDF() class into a
+    # variable pdf
+    pdf = FPDF()
+
+    # Add a page
+    pdf.add_page()
+
+    # set style and size of font
+    # that you want in the pdf
+    pdf.add_font("DejaVu", "", "DejaVuSans.ttf", uni=True)
+    pdf.set_font("DejaVu", size=15)
     for block in blocks:
-        print_block(block)
+        print_block(block, pdf)
+    # save the pdf with name .pdf
+    pdf.output("page.pdf")
 
 
-def print_block(block):
-    print(block.id)
-    print(block.has_children)
-    print(block.type)
+def print_block(block, pdf):
     if block.type in constants.BLOCK_TYPES:
-        print(block.content)
-
-    print("\n")
+        pdf.cell(200, 10, txt=block.content, ln=2, align='C')
